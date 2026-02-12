b529cafea6583de2e2bc965e4da84679908123e4
jakub <jakub@jakubserver.tail07dfef.ts.net>
2026-02-12 12:48:25 +0000

    fix: add TabNet, AutoGluon, FTTransformer to model imports
    - Added TabNetModel, AutoGluonModel, FTTransformerModel to __init__.py
    - Removed obsolete TCNModel and ChronosModel references
    - These models were implemented during refactoring but not exported

--- ml-training/ml_framework/models/__init__.py
+++ ml-training/ml_framework/models/__init__.py
@@ -1,7 +1,14 @@
 """Model implementations"""
 from .xgboost_model import XGBoostModel
 from .catboost_model import CatBoostModel
-from .tcn_model import TCNModel
-from .chronos_model import ChronosModel
+from .tabnet_model import TabNetModel
+from .autogluon_model import AutoGluonModel
+from .fttransformer_model import FTTransformerModel
 
-__all__ = ['XGBoostModel', 'CatBoostModel', 'TCNModel', 'ChronosModel']
+__all__ = [
+    'XGBoostModel',
+    'CatBoostModel',
+    'TabNetModel',
+    'AutoGluonModel',
+    'FTTransformerModel'
+]
