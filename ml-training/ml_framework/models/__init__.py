d0253d2527ce133dad66865bedd9920b79567c45
jakub <jakub@jakubserver.tail07dfef.ts.net>
2026-02-12 12:48:39 +0000

    fix: include all 6 models in __init__.py (TCN, Chronos)
    - Added TCNModel and ChronosModel back to imports
    - All model files exist and should be available

--- ml-training/ml_framework/models/__init__.py
+++ ml-training/ml_framework/models/__init__.py
@@ -4,11 +4,15 @@ from .catboost_model import CatBoostModel
 from .tabnet_model import TabNetModel
 from .autogluon_model import AutoGluonModel
 from .fttransformer_model import FTTransformerModel
+from .tcn_model import TCNModel
+from .chronos_model import ChronosModel
 
 __all__ = [
     'XGBoostModel',
     'CatBoostModel',
     'TabNetModel',
     'AutoGluonModel',
-    'FTTransformerModel'
+    'FTTransformerModel',
+    'TCNModel',
+    'ChronosModel'
 ]
